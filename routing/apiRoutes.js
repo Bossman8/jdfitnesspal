badc8bb6feafadb902e881d431f5118293f0559a
Bossman8 <jones.boston8@gmail.com>
2019-10-22 17:45:46 -0600

    added post methods to apiroutes

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -18,4 +18,22 @@ module.exports = function(app) {
       });
     }
   });
+
+  app.post("/api/new", (req, res) => {
+    User.create({
+      name: req.body.name,
+      number: req.body.number,
+      email: req.body.email,
+      weight: req.body.weight,
+      excercise: req.body.excercise
+    });
+  });
+
+  app.post("/api/delete", (req, res) => {
+    User.destroy({
+      where: {
+        id: req.body.id
+      }
+    });
+  });
 };
